9d80939912e1d54f0f11fcfebe52fbf78c67704a
Ali Shkeir <alishkeir_@hotmail.com>
2021-05-14 03:14:00 +0300

    added home details and design

--- src/components/Homes.test.js
+++ src/components/Homes.test.js
@@ -1,4 +1,4 @@
-import {act, getAllByTestId, render } from "@testing-library/react";
+import {act, getAllByTestId, getNodeText, render } from "@testing-library/react";
 import React from "react";
 import APIClient from "../services/APIClient";
 import Homes from "./Homes";
@@ -43,6 +43,47 @@ it("should show homes", () => {
   const homes = getAllByTestId(container, "home");
 
 
-  //~ Component should display at least 1 home
+  //~ Component should contain at least 1 home
   expect(homes.length).toBeGreaterThan(0);
+});
+
+
+it("should show home title", () => {
+  const homeTitles = getAllByTestId(container, "home-title");
+
+
+  //~ Component should contain specific home title
+  expect(getNodeText(homeTitles[0])).toBe('Home Test 1');
+});
+
+
+
+
+
+it("should show home image", () => {
+  const homeImages = getAllByTestId(container, "home-image");
+
+
+  //~ Component should have an image
+  expect(homeImages[0]).toBeTruthy();
+});
+
+
+
+
+it("should show home location", () => {
+  const homeLocations = getAllByTestId(container, "home-location");
+
+
+  //~ Component should contain specific home location
+  expect(getNodeText(homeLocations[0])).toBe('Test Location 1');
+});
+
+
+it("should show home location", () => {
+  const homePrices = getAllByTestId(container, "home-price");
+
+
+  //~ Component should contain specific home price
+  expect(getNodeText(homePrices[0])).toBe('$1/night');
 });
